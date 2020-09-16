bd6bab6eb051f60c53dc882634d3b2246344489d
qiaosiyi <qsy2011815@163.com>
2020-09-16 21:32:47 +0800

    Update eebpu12x12.v

--- HDL/dispatch/12x12/sources_1/imports/progASICp4SWITCH-master12/eebpu12x12.v
+++ HDL/dispatch/12x12/sources_1/imports/progASICp4SWITCH-master12/eebpu12x12.v
@@ -128,7 +128,7 @@ module eebpu12x12
 	assign newpkt_2c[11] = (input_sel == 11)?input_new_packet:0;
 
 	
-	assign in_data_2c[0] = (input_sel == 15)?input_data:0;
+	assign in_data_2c[0] = (input_sel == 0)?input_data:0;
 	assign in_data_2c[1] = (input_sel == 1)?input_data:0;
 	assign in_data_2c[2] = (input_sel == 2)?input_data:0;
 	assign in_data_2c[3] = (input_sel == 3)?input_data:0;
